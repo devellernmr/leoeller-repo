e489f79e8a054fe7855f9fe8a0fb9f4925bdee4b
LeoEller <devellernmr@gmail.com>
2025-03-25 17:20:22 -0300

    Adiciona componente Footer e estilos; ajusta estilos do componente de Contatos

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Projetos from "./components/Projects/Projects";
 import Contacts from "./components/Contacts/Contacts";
+import Footer from "./components/Footer/Footer";
 const App = () => {
 
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <About />
       <Projetos />
       <Contacts />
+      <Footer />
     </>
   );
 
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <section className="footerSection">
+      <div className="footerDiv">
+        <p>&copy; 2023 Meu Projeto Simples. Todos os direitos reservados.</p>
+      </div>
+    </section>
+  );
+};
+
+export default Footer;
